c8226e923382c25d0130021cabac582015106e8c
thomasaziz <thomas.aziz13@gmail.com>
2022-11-04 20:06:01 -0400

    add Annalyn's Infiltration exercise

---
+++ java/annalyns-infiltration/src/main/java/AnnalynsInfiltration.java
@@ -0,0 +1,37 @@
+class AnnalynsInfiltration {
+    public static boolean canFastAttack(boolean knightIsAwake) {
+        if (knightIsAwake){
+            return false;
+        }
+        else{
+            return true;
+        }
+    }
+
+    public static boolean canSpy(boolean knightIsAwake, boolean archerIsAwake, boolean prisonerIsAwake) {
+        if (knightIsAwake || archerIsAwake || prisonerIsAwake){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    public static boolean canSignalPrisoner(boolean archerIsAwake, boolean prisonerIsAwake) {
+        if (!archerIsAwake && prisonerIsAwake){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    public static boolean canFreePrisoner(boolean knightIsAwake, boolean archerIsAwake, boolean prisonerIsAwake, boolean petDogIsPresent) {
+        if ((petDogIsPresent && !archerIsAwake) || (!petDogIsPresent && prisonerIsAwake && !archerIsAwake && !knightIsAwake)){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+}
